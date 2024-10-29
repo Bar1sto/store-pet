0d0ab5fd1616728b5f2ca55a285a7120b5660139
Bar1sto <vovad3039@gmail.com>
2024-10-29 21:10:13 +0300

    29.10.2024 Изменение функций (FBVs) на классы (CBVs)

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from products.views import products, basket_add, basket_remove
+from products.views import ProdictsListView, basket_add, basket_remove
 
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('category/<int:category_id>/', products, name='category'),
-    path('page/<int:page_number>/', products, name='paginator'),
+    path('', ProdictsListView.as_view(), name='index'),
+    path('category/<int:category_id>/', ProdictsListView.as_view(), name='category'),
+    path('page/<int:page>/', ProdictsListView.as_view(), name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,28 +1,52 @@
+from lib2to3.fixes.fix_input import context
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
 from products.models import ProductCategory, Product, Basket
 from django.core.paginator import Paginator
 from users.models import User
 
 
-def index(request):
-    context = {
-        'title': 'Store',
-    }
-    return render(request, 'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
-    
-    context = {
-        'title': 'Store - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator,
-        }    
-    return render(request, 'products/products.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
+
+
+class ProdictsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super(ProdictsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self,*, object_list=None,**kwargs):
+        context = super(ProdictsListView,self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+# def products(request, category_id=None, page_number=1):
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     per_page = 3
+#     paginator = Paginator(products, per_page)
+#     products_paginator = paginator.page(page_number)
+#
+#     context = {
+#         'title': 'Store - Каталог',
+#         'categories': ProductCategory.objects.all(),
+#         'products': products_paginator,
+#         }
+#     return render(request, 'products/products.html', context)
 
 @login_required
 def basket_add(request, product_id):
